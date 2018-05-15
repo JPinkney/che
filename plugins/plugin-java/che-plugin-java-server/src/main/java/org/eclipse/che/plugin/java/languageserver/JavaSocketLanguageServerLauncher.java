1fda91028cb666ed728241fc80acb3016e70fd74
jpinkney <josh.pinkney@mail.utoronto.ca>
2018-05-15 10:48:28 -0400

    hold

--- plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaSocketLanguageServerLauncher.java
+++ plugins/plugin-java/che-plugin-java-server/src/main/java/org/eclipse/che/plugin/java/languageserver/JavaSocketLanguageServerLauncher.java
@@ -10,27 +10,19 @@
  */
 package org.eclipse.che.plugin.java.languageserver;
 
+import static org.eclipse.che.plugin.java.languageserver.JavaLanguageServerLauncher.createServerDescription;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.reflect.Proxy;
-import java.net.Socket;
-import java.util.Arrays;
-import javax.annotation.PreDestroy;
 import org.eclipse.che.api.languageserver.exception.LanguageServerException;
-import org.eclipse.che.api.languageserver.registry.DocumentFilter;
 import org.eclipse.che.api.languageserver.registry.LanguageServerDescription;
 import org.eclipse.che.api.languageserver.remote.CustomSocketLanguageServerLauncher;
 import org.eclipse.che.api.languageserver.remote.SocketLanguageServerLauncher;
 import org.eclipse.che.api.languageserver.service.FileContentAccess;
 import org.eclipse.che.api.languageserver.util.DynamicWrapper;
-import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.services.LanguageClient;
 import org.eclipse.lsp4j.services.LanguageServer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * This launcher allows jdt.ls to be wrapped by the jdt.ls extension when launched via socket
@@ -40,29 +32,12 @@ import org.slf4j.LoggerFactory;
 @Singleton
 public class JavaSocketLanguageServerLauncher extends SocketLanguageServerLauncher
     implements CustomSocketLanguageServerLauncher {
-  private static final Logger LOG = LoggerFactory.getLogger(JavaLanguageServerLauncher.class);
-  private int port;
-  private String host;
-  private Socket socket;
 
   private static final LanguageServerDescription DESCRIPTION = createServerDescription();
 
-  private ProcessorJsonRpcCommunication processorJsonRpcCommunication;
-
   @Inject
-  public JavaSocketLanguageServerLauncher(
-      ProcessorJsonRpcCommunication processorJsonRpcCommunication) {
-    super(null, "", 0);
-    this.processorJsonRpcCommunication = processorJsonRpcCommunication;
-  }
-
-  @PreDestroy
-  public void closeSocket() {
-    try {
-      socket.close();
-    } catch (IOException e) {
-
-    }
+  public JavaSocketLanguageServerLauncher() {
+    super(JavaLanguageServer.class, DESCRIPTION, "", 0);
   }
 
   @Override
@@ -73,73 +48,15 @@ public class JavaSocketLanguageServerLauncher extends SocketLanguageServerLaunch
   @Override
   public LanguageServer launch(String projectPath, LanguageClient client)
       throws LanguageServerException {
-    try {
-      socket = new Socket(host, port);
-      socket.setKeepAlive(true);
-      InputStream inputStream = socket.getInputStream();
-      OutputStream outputStream = socket.getOutputStream();
 
-      Object javaLangClient =
-          Proxy.newProxyInstance(
-              getClass().getClassLoader(),
-              new Class[] {LanguageClient.class, JavaLanguageClient.class},
-              new DynamicWrapper(this, client));
-
-      Launcher<JavaLanguageServer> launcher =
-          Launcher.createLauncher(
-              javaLangClient, JavaLanguageServer.class, inputStream, outputStream);
-      launcher.startListening();
-      JavaLanguageServer proxy = launcher.getRemoteProxy();
-      LanguageServer wrapped =
-          (LanguageServer)
-              Proxy.newProxyInstance(
-                  getClass().getClassLoader(),
-                  new Class[] {LanguageServer.class, FileContentAccess.class},
-                  new DynamicWrapper(new JavaLSWrapper(proxy), proxy));
-      return wrapped;
-    } catch (IOException e) {
-      throw new LanguageServerException(
-          "Can't launch language server for project: " + projectPath, e);
-    }
-  }
-
-  public void sendStatusReport(StatusReport report) {
-    LOG.info("{}: {}", report.getType(), report.getMessage());
-  }
-
-  /**
-   * The show message notification is sent from a server to a client to ask the client to display a
-   * particular message in the user interface.
-   *
-   * @param report information about report
-   */
-  public void sendProgressReport(ProgressReport report) {
-    processorJsonRpcCommunication.sendProgressNotification(report);
-  }
-
-  private static LanguageServerDescription createServerDescription() {
-    LanguageServerDescription description =
-        new LanguageServerDescription(
-            "org.eclipse.che.plugin.java.languageserver",
-            Arrays.asList("javaSource", "javaClass"),
-            Arrays.asList(
-                new DocumentFilter(null, null, "jdt"), new DocumentFilter(null, null, "chelib")),
-            Arrays.asList(
-                "glob:**/*.java",
-                "glob:**/pom.xml",
-                "glob:**/*.gradle",
-                "glob:**/.project",
-                "glob:**/.classpath",
-                "glob:**/settings/*.prefs"));
-    return description;
-  }
-
-  public void setPort(int port) {
-    this.port = port;
-  }
-
-  public void setHost(String host) {
-    this.host = host;
+    JavaLanguageServer proxy = super.launch(projectPath, client);
+    LanguageServer wrapped =
+        (LanguageServer)
+            Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[] {LanguageServer.class, FileContentAccess.class},
+                new DynamicWrapper(new JavaLSWrapper(proxy), proxy));
+    return wrapped;
   }
 
   public LanguageServerDescription getDescription() {
